27e940ff38df28ff60e1d24575b08636b35ccd89
sumitsharansatsangi <sharansumitkumar@gmail.com>
2024-10-01 19:18:06 +0530

    code updated

---
+++ CHANGELOG.md
@@ -0,0 +1,32 @@
+## 1.6.8
+
+- Adds support for `web: ^1.0.0`
+
+## 1.6.7
+
+- Migrate from dart:js to web package for Wasm support (#102 by [san-smith](https://github.com/san-smith))
+
+## 1.6.6
+
+- Added Web support (#95 by [san-smith](https://github.com/san-smith))
+
+## 1.6.5
+
+- Update package's dart SDK max version (under 3.0.0)
+
+## 1.6.4
+
+- Fix BinaryMessenger initialization.
+- Unify behavior between iOS and Android versions (#51 by [Leicas](https://github.com/Leicas))
+
+## 1.6.3-nullsafety.0
+
+- Migrating to null safety.
+
+## 1.6.2
+
+- Fix building on iOS.
+
+## 1.6.1
+
+- Added Web support (#43 by [roulljdh](https://github.com/roulljdh))
---
+++ lib/vibration_web.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+import 'dart:js_interop';
+import 'dart:js_interop_unsafe';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:web/web.dart' as web;
+
+class VibrationWebPlugin {
+  /// Get navigator JS object
+  static final _navigator = web.window.navigator;
+
+  static void registerWith(Registrar registrar) {
+    final MethodChannel channel = MethodChannel(
+      'vibration',
+      const StandardMethodCodec(),
+      registrar,
+    );
+    final VibrationWebPlugin pluginInstance = VibrationWebPlugin();
+
+    channel.setMethodCallHandler(pluginInstance.handleMethodCall);
+  }
+
+  /// Handles method calls over the MethodChannel of this plugin.
+  /// Note: Check the "federated" architecture for a new way of doing this:
+  /// https://flutter.dev/go/federated-plugins
+  Future<dynamic> handleMethodCall(MethodCall call) async {
+    switch (call.method) {
+      case 'hasVibrator':
+        return Future.value(_hasVibrator());
+      case 'vibrate':
+        final int duration = call.arguments['duration'];
+        final List<int> pattern = call.arguments['pattern'].cast<int>();
+
+        return Future.value(_vibrate(duration: duration, pattern: pattern));
+      case 'hasAmplitudeControl':
+      case 'hasCustomVibrationsSupport':
+        return Future.value(false);
+      case 'cancel':
+        return Future.value(_cancel());
+      default:
+        throw PlatformException(
+          code: 'Unimplemented',
+          details: 'vibration_web doesn\'t implement \'${call.method}\'',
+        );
+    }
+  }
+
+  static bool _hasVibrator() {
+    /// Check if the navigator object has the vibrate function
+    return _navigator.has('vibrate');
+  }
+
+  static _vibrate({int duration = 500, List<int> pattern = const []}) {
+    if (_hasVibrator()) {
+      /// If pattern is not empty, convert to JS array
+      final args =
+          pattern.length > 0 ? pattern.jsify() ?? duration.toJS : duration.toJS;
+
+      /// Call vibrate function
+      _navigator.vibrate(args);
+    }
+  }
+
+  static _cancel() {
+    _vibrate(duration: 0);
+  }
+}
